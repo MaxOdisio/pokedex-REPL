26f3215102e89b873c4f6e5a84519ef9e0c12405
Max <massimo.odisio@gmail.com>
2025-11-19 17:52:35 +0100

    Applicazione regole gitignore e rimozione file ignorati

--- dist/command_exit.js
+++
@@ -1,7 +0,0 @@
-export async function commandExit(state) {
-    console.log("Closing the Pokedex... Goodbye!");
-    state.readline.close();
-    state.pokeAPI.closeCache();
-    process.exit(0);
-}
-;
